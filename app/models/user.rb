a34813a346f367742ea3bd5d695ab50dc1a4943e
Cole Horvat <colehorvat@pop-os.localdomain>
2022-08-08 02:13:27 -0700

    Added friends list filtering in controller, model relationships, modified friends list

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   validates_uniqueness_of :email
+  has_many :friends
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
